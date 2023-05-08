b38446a15db798df48c9544fdc58fc8c0592c67c
Rikicavaz77 <riccardo.cavalli@studenti.unipd.it>
2023-05-08 13:47:38 +0200

    Inizializzate classi base del progetto
    close #4

---
+++ src/main/java/it/unipd/mtss/business/IntegerToRoman.java
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////////////////
+// Riccardo Cavalli 2042893
+// Pietro Giovanni Marchiorato 2042365
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss.business;
+
+public class IntegerToRoman {
+
+    public static String convert(int number) {
+        
+        return null;
+    }
+      
+}
